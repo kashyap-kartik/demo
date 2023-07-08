22a914513f3b23df2e434849c30bf2d7284c219b
kashyap-kartik <138874988+kashyap-kartik@users.noreply.github.com>
2023-07-08 18:49:09 +0530

    Update main.tf

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -82,7 +82,7 @@ resource "azurerm_virtual_machine" "app-vm" {
   location = var.location
   resource_group_name = var.resource_group
   network_interface_ids = [ azurerm_network_interface.app-net-interface.id ]
-  availability_set_id = azurerm_availability_set.web_availabilty_set.id
+  availability_set_id = azurerm_availability_set.app_availabilty_set.id
   vm_size = "Standard_D2s_v3"
   delete_os_disk_on_termination = true
   
